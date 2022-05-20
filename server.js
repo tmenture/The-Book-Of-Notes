d4ab9112245e4e798b41d5c867cea0c9d467960e
tmenture <thomasoxemail@gmail.com>
2022-05-20 14:54:04 -0400

    note data now displays to page

--- server.js
+++ server.js
@@ -23,10 +23,17 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+// Gets and sends note data to the page
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'));
+});
+
 // Gets and links html files for homepage if a non-existent api call is made
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
+// 
 
 // Listens for server start
 app.listen(PORT, () => {
